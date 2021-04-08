02ec56368e0270e4227afb1ee1edb78397dd4353
nassir <daniya_n@yahoo.com>
2021-04-07 19:19:14 -0700

    first commit

--- hub/hub.js
+++ hub/hub.js
@@ -10,54 +10,46 @@ const support = io.of('/support');
 
 
 const orderQueue = {
-  recived: {},
+  received: {},
   inprogress: {},
   completed: {},
 };
-
-
 support.on('connection', (socket) => {
   console.log('Support connection created ', socket.id);
   socket.on('help-need', payload => {
-    orderQueue.recived[payload.supportID] = payload;
-    logger('help-need', payload)
-    socket.broadcast.emit('help-need', payload);
+    orderQueue.received[payload.supportID] = payload;
+//  logger('help-need',payload)
+ console.log('EVENT', payload);
+socket.emit('help-need', payload);
   });
-
   socket.on('inprogress', payload => {
     orderQueue.inprogress[payload.supportID] = payload;
-    console.log('inprogress', payload);
-    socket.emit('helping', payload);
+  //  logger('inprogress',payload)
+    socket.emit('help-need', payload); 
+    console.log('EVENT', payload);
   });
-
   socket.on('received', payload => {
-    delete orderQueue.recived[payload.supportID];
+    delete orderQueue.received[payload.supportID];
     delete orderQueue.inprogress[payload.supportID];
     delete orderQueue.completed[payload.supportID];
     support.emit('completed', payload);
   });
-
   socket.on('getAll', () => {
-    for (let key in orderQueue.reveived) {
-      socket.emit('help-need', orderQueue.recived[key]);
+    for(let key in orderQueue.reveived) {
+      socket.emit('help-need', orderQueue.received[key]);
     }
-    for (let key in orderQueue.inprogress) {
+    for(let key in orderQueue.inprogress) {
       socket.emit('inprogress', orderQueue.inprogress[key]);
     }
-    for (let key in orderQueue.completed) {
+    for(let key in orderQueue.completed) {
       socket.emit('completed', orderQueue.completed[key]);
     }
   });
-
   socket.on('completed', payload => {
     orderQueue.completed[payload.supportID] = payload;
-    console.log('help need completed', payload);
+    console.log('EVENT:', payload);
+    socket.broadcast.emit('completed', payload);
+     support.emit('completed', { supportID: payload.supportID, payload: payload});
   });
-});
-
-
-// function logger('', payload) {
-//   console.log(`${event, payload} ON ${new Date()}`);
-// };
-
+})
 console.log('HUB-LIVE');
--- support/support.js
+++ support/support.js
@@ -1,54 +1,43 @@
 'use strict';
 
-// //suport//=== driver====
-// const io = require('socket.io-client');
-// const capsURL = 'http://localhost:3000/support';
-// const socket = io.connect(capsURL);
-// socket.emit('getAll');
-// socket.on('help-need', (payload) => {
-//   setTimeout(() => {
-//     socket.emit('received', payload);
-//     console.log(`Picking up ${payload.order.orderId}`);
-//     payload.event = 'in-transit';
-//     socket.emit('in-transit', payload);
-//   }, 1500);
-// });
-// socket.on('in-transit', (payload) => {
-//   setTimeout(() => {
-//     socket.emit('received', payload);
-//     payload.event = 'delivered';
-//     socket.emit('delivered', payload);
-//   }, 3000);
-// });
-// console.log('***SUPPORT LIVE***');
-
 require('dotenv').config();
+const faker = require('faker');
 const io = require('socket.io-client');
-const capsURL = 'http://localhost:3000/support';
-const socket = io.connect(capsURL);
+const studentURL = 'http://localhost:3000/support';
+
+const socket = io.connect(studentURL);
+
+
 
 socket.emit('getAll');
 
 socket.on('help-need', (payload) => {
+
   setTimeout(() => {
     socket.emit('received', payload);
-    console.log(`${payload.studentName} is requesting help at table ${payload.tableNumber}`);
+    console.log(`${payload.studentName} is requesting help at table# ${payload.tableNumber}`);
+    
     socket.emit('inprogress', payload);
-  }, 5000);
+  },5000);
+
 });
 
 socket.on('helping', (payload) => {
+  
   setTimeout(() => {
     socket.emit('received', payload);
-    socket.emit('complete', payload);
-  }, 6000);
-});
+    
+    socket.emit('completed', payload);
+  },6000);
 
+});
 socket.on('conclusion', (payload) => {
+  
   setTimeout(() => {
-    console.log(`nice!${payload.TA}, DONE!!`)
-  }, 6000);
-});
+    
+  console.log(`nice!${payload.TA}, DONE!!`)
+  },6000);
 
+});
 console.log('SUPPORT-LIVE');
 
